10188200b45e35db8ee920bfa6840eb1a5abbe15
rizqyazhar <rizqytask20@gmail.com>
2025-03-02 11:55:50 +0700

    fix navbar LinkTo

--- src/components/fragments/Navbar.jsx
+++ src/components/fragments/Navbar.jsx
@@ -1,17 +1,18 @@
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <>
         <nav className="max-w-screen px-20 py-6 bg-pageHeaderBackground flex justify-between">
             <div className="flex items-center gap-20">
-              <div>
+              <Link to="/home">
                 <img src="/public/logo/chill-logo.svg" className="w-[103.55px]" />
-              </div>
+              </Link>
               <div className="font-lato font-medium text-lg tracking-[.2px] text-white flex gap-20">
-                <a href="">Series</a>
-                <a href="">Film</a>
-                <a href="">Daftar Saya</a>
+                <Link to="/series">Series</Link>
+                <Link to="/films">Film</Link>
+                <Link to="/mylist">Daftar Saya</Link>
               </div>
             </div>
             <div className="flex gap-2 items-center">
