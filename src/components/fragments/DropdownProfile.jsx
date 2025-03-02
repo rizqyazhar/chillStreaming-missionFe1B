23b75510e6293e7b0680cdf3d46e4c1e6ad2dc5d
rizqyazhar <rizqytask20@gmail.com>
2025-03-03 06:55:01 +0700

    fix div in dropdown profile

--- src/components/fragments/DropdownProfile.jsx
+++ src/components/fragments/DropdownProfile.jsx
@@ -6,18 +6,18 @@ import { Link } from "react-router-dom"
 const DropdownProfile = () => {
   return (
     <div className="absolute -bottom-[100px] md:-bottom-[132px] -right-2 md:-right-5 z-20 w-32 md:w-[156px] rounded-sm py-1 font-lato text-white bg-pageHeaderBackground">
-        <div className="flex items-center w-full  hover:bg-extraBackground transition duration-700 gap-1.5 px-3 py-2">
-            < IoPerson className="text-sm md:text-lg" />
-            <Link to="" className="text-xs md:text-sm">Profil Saya</Link>
-        </div>
-        <div className="flex items-center w-full  hover:bg-extraBackground transition duration-700 gap-1.5 px-3 py-2">
-            < MdOutlineStar className="text-sm md:text-lg" />
-            <Link to="" className="text-xs md:text-sm">Ubah Premium</Link>
-        </div>
-        <div className="flex items-center w-full  hover:bg-extraBackground transition duration-700 gap-1.5 px-3 py-2">
-            < RiLogoutBoxRLine className="text-sm md:text-lg" />
-            <Link to="/" className="text-xs md:text-sm">Keluar</Link>
-        </div>
+        <Link to="" className="text-xs md:text-sm flex items-center w-full  hover:bg-extraBackground transition duration-700 gap-1.5 px-3 py-2">
+          < IoPerson className="text-sm md:text-lg" />
+          Profil Saya
+        </Link>
+        <Link to="" className="text-xs md:text-sm flex items-center w-full  hover:bg-extraBackground transition duration-700 gap-1.5 px-3 py-2">
+          < MdOutlineStar className="text-sm md:text-lg" />
+          Ubah Premium
+        </Link>
+        <Link to="/" className="text-xs md:text-sm flex items-center w-full  hover:bg-extraBackground transition duration-700 gap-1.5 px-3 py-2">
+          < RiLogoutBoxRLine className="text-sm md:text-lg" />
+          Keluar
+        </Link>
     </div>
   )
 }
