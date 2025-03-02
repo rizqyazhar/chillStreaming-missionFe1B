cf08b8b5790944e68969cf48fb06e9a6bbd875a9
rizqyazhar <rizqytask20@gmail.com>
2025-03-02 11:50:51 +0700

    fix toggle genre dropdown

--- src/components/elements/genres/Genre.jsx
+++ src/components/elements/genres/Genre.jsx
@@ -1,12 +1,18 @@
 import { MdOutlineKeyboardArrowDown } from "react-icons/md"
 import GenreList from "./GenreList"
+import { useState } from "react";
 
 const Genre = () => {
+  const [showGenre, setShowGenre] = useState(false);
   return (
-    <div className="relative flex justify-center items-center gap-2 w-28 h-11 rounded-lg mb-24 bg-paperBackground cursor-pointer">
+    <div className="relative flex justify-center items-center gap-2 w-28 h-11 rounded-lg mb-24 bg-paperBackground cursor-pointer" onClick={ (e) => {
+      e.preventDefault();
+      setShowGenre(!showGenre)
+      
+      } }>
         <p className="font-bold tracking-[.2px]">Genre</p>
         <MdOutlineKeyboardArrowDown className="text-xs w-6 h-6"/>
-        <GenreList />
+        { showGenre && <GenreList /> }
     </div>
   )
 }
