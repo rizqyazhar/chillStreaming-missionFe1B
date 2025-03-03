173cfc4ea8035237dcbe0a10b04549abc1905c82
rizqyazhar <rizqytask20@gmail.com>
2025-03-03 08:02:55 +0700

    fix url img and overflow

--- src/components/elements/button/buttonFormGoogle.jsx
+++ src/components/elements/button/buttonFormGoogle.jsx
@@ -4,7 +4,7 @@ const ButtonFormGoogle = ({ text }) => {
         className="w-full px-3 md:px-5 py-2 md:py-3.5 flex justify-center items-center gap-3 md:gap-5 bg-transparent text-white border border-outlineBorder tracking-[.2px] font-lato font-[600] text-[10px] md:text-[16px] rounded-3xl cursor-pointer"
     >
       <img 
-        src="/public/logo/google-icon.svg" 
+        src="/logo/google-icon.svg" 
         className="w-2.5 md:w-[18px]"
       />
       <span 
--- src/components/fragments/LandscapeBoxs.jsx
+++ src/components/fragments/LandscapeBoxs.jsx
@@ -16,7 +16,7 @@ const LandscapeBoxs = ({ title }) => {
         <div className="absolute right-4 bottom-[99px] w-11 h-11 hidden md:flex md:justify-center md:items-center rounded-full border border-outlineBorder bg-bodyBackground cursor-pointer">
           <FaArrowRight className="text-lg text-white"/>
         </div>
-        <div className="w-full h-40 px-5 md:px-10 grid grid-cols-[repeat(4,minmax(95px,309px))] grid-rows-1 md:grid-cols-[repeat(4,minmax(95px,302px))] gap-x-56 md:gap-x-6 overflow-x-auto">
+        <div className="w-full h-40 px-5 md:px-10 grid grid-cols-[repeat(4,minmax(95px,309px))] grid-rows-1 md:grid-cols-[repeat(4,minmax(95px,302px))] gap-x-56 md:gap-x-6 overflow-x-auto md:overflow-auto">
             {landscape.map((l, index) => (
               index < 4 && <LandscapeBox key={l.id} src={l.img} />
             ))}
